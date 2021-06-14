e301065c2268b36916c98b4f7e596800d031b56a
Nshuti Jabes <nshutij7@gmail.com>
2021-06-14 07:26:08 +0100

    reserved

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -9,6 +9,9 @@ class User extends CI_Controller {
     public function login_view(){
         $this->load->view('user/login');
     }
+    public function resources(){
+        $this->load->view('resources');
+    }
     public function login() {
        $this->form_validation->set_rules('email','Email',
        'trim|required|valid_email', 
